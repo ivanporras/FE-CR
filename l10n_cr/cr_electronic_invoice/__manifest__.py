5ad3378d4ff1b894406bc858ec8a0803cb634369
Ivan Porras <ivanporraslopez@gmail.com>
2025-08-15 14:19:25 -0600

    [IKA] Change for upgrade v17

--- l10n_cr/cr_electronic_invoice/__manifest__.py
+++ l10n_cr/cr_electronic_invoice/__manifest__.py
@@ -65,7 +65,7 @@
         'views/account_move_reversal_views.xml',
         'views/report_invoice.xml',
         'views/account_portal_templates.xml',
-        'views/web_external_layout_striped.xml',
+        #'views/web_external_layout_striped.xml',
         'security/ir.model.access.csv',
         # 'data/account_tax_template_data.xml',
     ],
